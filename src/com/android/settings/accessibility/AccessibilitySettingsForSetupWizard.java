CalyxOS/platform_packages_apps_Settings

Show TalkBack under vision accessibility settings during setup
Uses different package name to avoid conflicting with Google closed-source variant

Change-Id: If3aa0fc4aaf6e0d4cff17fa0c3c10a7c484282ae

src/com/android/settings/accessibility/AccessibilitySettingsForSetupWizard.java
@@ -67,12 +67,12 @@ public class AccessibilitySettingsForSetupWizard extends DashboardFragment
 
     // Package names and service names used to identify screen reader and SelectToSpeak services.
     @VisibleForTesting
-    static final String SCREEN_READER_PACKAGE_NAME = "com.google.android.marvin.talkback";
+    static final String SCREEN_READER_PACKAGE_NAME = "com.android.talkback";
     @VisibleForTesting
     static final String SCREEN_READER_SERVICE_NAME =
             "com.google.android.marvin.talkback.TalkBackService";
     @VisibleForTesting
-    static final String SELECT_TO_SPEAK_PACKAGE_NAME = "com.google.android.marvin.talkback";
+    static final String SELECT_TO_SPEAK_PACKAGE_NAME = "com.android.talkback";
     @VisibleForTesting
     static final String SELECT_TO_SPEAK_SERVICE_NAME =
             "com.google.android.accessibility.selecttospeak.SelectToSpeakService";

CharaROMAndroid/android_packages_apps_Settings

src/com/android/settings/accessibility/AccessibilitySettingsForSetupWizard.java
@@ -69,12 +69,12 @@ public class AccessibilitySettingsForSetupWizard extends DashboardFragment
 
     // Package names and service names used to identify screen reader and SelectToSpeak services.
     @VisibleForTesting
-    static final String SCREEN_READER_PACKAGE_NAME = "com.google.android.marvin.talkback";
+    static final String SCREEN_READER_PACKAGE_NAME = "org.calyxos.talkback";
     @VisibleForTesting
     static final String SCREEN_READER_SERVICE_NAME =
             "com.google.android.marvin.talkback.TalkBackService";
     @VisibleForTesting
-    static final String SELECT_TO_SPEAK_PACKAGE_NAME = "com.google.android.marvin.talkback";
+    static final String SELECT_TO_SPEAK_PACKAGE_NAME = "org.calyxos.talkback";
     @VisibleForTesting
     static final String SELECT_TO_SPEAK_SERVICE_NAME =
             "com.google.android.accessibility.selecttospeak.SelectToSpeakService";